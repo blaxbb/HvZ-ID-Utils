7642a6afcf2cff0fff9c750cdb5f7c53e5b8a30b
Brian Barrett <blaxbb@gmail.com>
2010-06-15 03:26:46 -0400

    Added HvZMod to source tree
    Created Batch ID Generation to HvZMod

---
+++ HvZmod/HvZmod/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace HvZmod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label_Save = new System.Windows.Forms.Label();
+            this.label_outLoc = new System.Windows.Forms.Label();
+            this.label_list = new System.Windows.Forms.Label();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_outLocation = new System.Windows.Forms.Button();
+            this.button_list = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(284, 240);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label_Save);
+            this.tabPage1.Controls.Add(this.label_outLoc);
+            this.tabPage1.Controls.Add(this.label_list);
+            this.tabPage1.Controls.Add(this.button_save);
+            this.tabPage1.Controls.Add(this.button_outLocation);
+            this.tabPage1.Controls.Add(this.button_list);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(276, 214);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Generate";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label_Save
+            // 
+            this.label_Save.AutoSize = true;
+            this.label_Save.Location = new System.Drawing.Point(106, 72);
+            this.label_Save.Name = "label_Save";
+            this.label_Save.Size = new System.Drawing.Size(0, 13);
+            this.label_Save.TabIndex = 3;
+            // 
+            // label_outLoc
+            // 
+            this.label_outLoc.AutoSize = true;
+            this.label_outLoc.Location = new System.Drawing.Point(107, 42);
+            this.label_outLoc.Name = "label_outLoc";
+            this.label_outLoc.Size = new System.Drawing.Size(0, 13);
+            this.label_outLoc.TabIndex = 3;
+            // 
+            // label_list
+            // 
+            this.label_list.AutoSize = true;
+            this.label_list.Location = new System.Drawing.Point(107, 12);
+            this.label_list.Name = "label_list";
+            this.label_list.Size = new System.Drawing.Size(0, 13);
+            this.label_list.TabIndex = 3;
+            // 
+            // button_save
+            // 
+            this.button_save.Enabled = false;
+            this.button_save.Location = new System.Drawing.Point(9, 67);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(91, 23);
+            this.button_save.TabIndex = 2;
+            this.button_save.Text = "Batch Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // button_outLocation
+            // 
+            this.button_outLocation.Enabled = false;
+            this.button_outLocation.Location = new System.Drawing.Point(9, 37);
+            this.button_outLocation.Name = "button_outLocation";
+            this.button_outLocation.Size = new System.Drawing.Size(91, 23);
+            this.button_outLocation.TabIndex = 1;
+            this.button_outLocation.Text = "Output Location";
+            this.button_outLocation.UseVisualStyleBackColor = true;
+            this.button_outLocation.Click += new System.EventHandler(this.button_imageOutLocation_Click);
+            // 
+            // button_list
+            // 
+            this.button_list.Location = new System.Drawing.Point(9, 7);
+            this.button_list.Name = "button_list";
+            this.button_list.Size = new System.Drawing.Size(91, 23);
+            this.button_list.TabIndex = 0;
+            this.button_list.Text = "Master List";
+            this.button_list.UseVisualStyleBackColor = true;
+            this.button_list.Click += new System.EventHandler(this.button_list_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(276, 214);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Attendance";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(276, 214);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Create List";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label_Save;
+        private System.Windows.Forms.Label label_outLoc;
+        private System.Windows.Forms.Label label_list;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Button button_outLocation;
+        private System.Windows.Forms.Button button_list;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ HvZmod/HvZmod/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using com.google.zxing;
+using com.google.zxing.qrcode;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+using HtmlAgilityPack;
+using System.Reflection;
+using System.IO;
+
+
+namespace HvZmod
+{
+    public partial class Form1 : Form
+    {
+        string htmlLocation = "";
+        string imageSaveLocation = "";
+
+        List<player> players = new List<player>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("players.csv"))
+            {
+                putCSV(new StreamReader("players.csv"));
+                enableControls();
+
+            }
+        }
+
+        private void enableControls()
+        {
+            button_outLocation.Enabled = true;
+            
+        }
+
+        public class player
+        {
+            public string name;
+            public string killid;
+            public string email;
+
+            public player(string name, string killid, string email)
+            {
+                this.name = name;
+                this.killid = killid;
+                this.email = email;
+            }
+        }
+
+        private void button_list_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK
+                && openFileDialog1.FileName.Length > 0)
+            {
+                toolStripStatusLabel1.Text = "HTML Loaded";
+                htmlLocation = openFileDialog1.FileName.ToString();
+                
+                
+                string[] parsedHtml = parseHtml(htmlLocation);
+
+                int skippedHeader = 0;
+
+                foreach (string parse in parsedHtml)
+                {
+                    if (skippedHeader == 0)
+                    {
+                        skippedHeader++;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            string[] splitParse = (parse.ToString()).Split('|');
+                            players.Add(new player(splitParse[0], splitParse[1],splitParse[2]));                         
+                        }
+                        catch (Exception excep)
+                        {
+                            MessageBox.Show(excep.Message);
+                            break;
+                        }
+
+
+
+                    }
+                    
+                }
+
+                StreamWriter sw = new StreamWriter("players.csv");
+                sw.WriteLine("Name,KillID,Email");
+                players.ForEach(delegate(player p)
+                {
+                    sw.WriteLine(p.name + "," + p.killid + "," + p.email);
+                });
+                sw.Close();
+                label_list.Text = htmlLocation;
+
+                button_outLocation.Enabled = true;
+            }
+        }
+
+        private void button_imageOutLocation_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK
+                && folderBrowserDialog1.SelectedPath.ToString().Length > 0)
+            {
+                toolStripStatusLabel1.Text = "Folder Selected";
+                imageSaveLocation = folderBrowserDialog1.SelectedPath.ToString();
+                label_outLoc.Text = imageSaveLocation;
+                button_save.Enabled = true;
+            }
+        }
+        
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Processing";
+
+            players.ForEach(delegate(player p)
+            {
+                generateID(p.name, p.killid).Save(imageSaveLocation + @"\" + p.name + "_ID.jpeg", ImageFormat.Jpeg);
+                Console.WriteLine("Generated " + p.name + "'s ID");
+            });
+            //string[] parsedHtml =  parseHtml(htmlLocation);
+            
+            //int skippedHeader = 0;
+
+            //foreach (string parse in parsedHtml)
+            //{
+            //    if (skippedHeader ==0)
+            //    {
+            //        skippedHeader++;
+            //    }
+            //    else
+            //    {
+            //        try
+            //        {
+            //            string[] splitParse = (parse.ToString()).Split('|');
+            //            Console.WriteLine("Generating" + splitParse[0]);
+            //            generateID(splitParse[0], splitParse[1]).Save(imageSaveLocation + @"\" + splitParse[0] + "_ID.jpeg", ImageFormat.Jpeg);
+            //        }
+            //        catch (Exception excep)
+            //        {
+            //            MessageBox.Show(excep.Message);
+            //            return;
+            //        }
+            //    }
+            //}
+
+        }
+
+        private String[] parseHtml(string filename)
+        {
+            
+            
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            
+            doc.Load(filename);
+
+            String[] procTable = new string[5000];
+            int rowNumb = 0;
+
+            StringBuilder sb = new StringBuilder();           
+            
+
+            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table"))
+            {
+                Console.WriteLine("Found: " + table.Id);
+                foreach (HtmlNode row in table.SelectNodes("tr"))
+                {
+                    Console.WriteLine("row "+ rowNumb.ToString());
+                    
+                    foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                    {
+
+                        
+                        Console.WriteLine("cell: " + cell.InnerText);
+
+                        sb.Append(cell.InnerText);
+                        sb.Append("|");
+                    }
+                    sb.Remove((sb.Length - 1), 1);
+                    procTable[rowNumb] = sb.ToString();
+                    sb.Remove(0, sb.Length);
+                    rowNumb++;
+                }
+            }
+
+            return procTable;
+        }
+
+        public static string getCSV<T>(List<string> list)
+        {
+            string[] array = list.ToArray();
+            return (string.Join(",", array));
+        }
+
+        public void putCSV(StreamReader file)
+        {
+            try
+            {
+
+                int firstLine = 0;
+                string line;
+                string[] row;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (firstLine == 0) { firstLine++; }
+                    else
+                    {
+                        row = line.Split(',');
+                        players.Add(new player(row[0], row[1], row[2]));
+                    }
+                }
+
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+
+        }
+
+        private Image generateID(string name, string ID)
+        {
+            String encoding = name + "|" + ID;
+
+            com.google.zxing.qrcode.QRCodeWriter qrCode = new com.google.zxing.qrcode.QRCodeWriter();
+            com.google.zxing.common.ByteMatrix byteIMG = qrCode.encode(encoding, com.google.zxing.BarcodeFormat.QR_CODE, 200, 200);
+
+            sbyte[][] img = byteIMG.Array;
+
+            Bitmap qrCodeBmp = new Bitmap(200, 200);
+            Graphics g = Graphics.FromImage(qrCodeBmp);
+
+            g.Clear(Color.White);
+
+            for (int i = 0; i <= img.Length - 1; i++)
+            {
+                for (int j = 0; j <= img[i].Length - 1; j++)
+                {
+                    if (img[i][j] == 0)
+                    {
+                        g.FillRectangle(Brushes.Black, j, i, 1, 1);
+                    }
+                    else
+                    {
+                        g.FillRectangle(Brushes.White, j, i, 1, 1);
+                    }
+                }
+            }
+
+
+            Bitmap blankID = new Bitmap("hvzid02.jpg");
+            Bitmap genID = new Bitmap(234, 126);
+
+            Graphics build = Graphics.FromImage(genID);
+            build.DrawImage(blankID, 0, 0);
+
+
+
+            build.DrawString(name, new Font("Arial", 10), new SolidBrush(System.Drawing.Color.Black), 6, 80);
+            build.DrawString(ID, new Font("Arial", 10), new SolidBrush(System.Drawing.Color.Black), 6, 95);
+
+            build.DrawImage(scaleByPercent(qrCodeBmp, 75), 95, -12);
+
+            toolStripStatusLabel1.Text = "Encoded";
+
+            return genID;
+        }
+
+        static Image scaleByPercent(Image imgPhoto, int Percent)
+        {
+            float nPercent = ((float)Percent / 100);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+
+            int destX = 0;
+            int destY = 0;
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(destWidth, destHeight,
+                                     PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution,
+                                    imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+    }
+}
